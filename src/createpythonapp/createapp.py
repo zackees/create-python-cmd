3b06ad1924a30b86a70aac17283c0cd0f9f07674
zackees <z@zackees.com>
2023-01-30 23:35:19 -0800

    make it work

--- src/createpythonapp/createapp.py
+++ src/createpythonapp/createapp.py
@@ -7,7 +7,8 @@
 
 import os
 import tempfile
-from shutil import which, rmtree
+import shutil
+from typing import Optional
 
 
 def check_name(app_name: str) -> None:
@@ -24,25 +25,48 @@ def check_semantic_version(version: str) -> None:
             raise ValueError("The version of the application is not a valid semantic version.")
 
 
+def remove_double_blank_lines(lines: list) -> list:
+    """Remove double blank lines."""
+    new_lines = []
+    last_line_blank = False
+    for i, line in enumerate(lines):
+        if line == "":
+            if last_line_blank:
+                continue
+            new_lines.append(line)
+            last_line_blank = True
+        else:
+            new_lines.append(line)
+            last_line_blank = False
+    return new_lines
+
+
 def do_create_python_app(
-    app_name: str,
     app_description: str,
     app_author: str,
     version: str,
     github_url: str,
-    add_commmand: bool,
+    command_name: Optional[str] = None,
+    cwd: Optional[str] = None,
 ) -> None:
     # Create the app directory
-    os.makedirs(app_name, exist_ok=True)
+    # get app name from the github url
+    cwd = cwd or os.getcwd()
+    app_name = github_url.split("/")[-1]
+    app_name_underscore = app_name.replace("-", "_")
     with tempfile.TemporaryDirectory() as tmpdir:
         # download https://github.com/zackees/template-python-cmd
         # extract to tmpdir
         # copy files from tmpdir to app_name
         os.system(f"git clone https://github.com/zackees/template-python-cmd {tmpdir}")
-        print(os.listdir(tmpdir))
+        # change every directory name of from template-python-cmd to app_name
+        for root, dirs, files in os.walk(tmpdir):
+            for d in dirs:
+                if d == "template-python-cmd" or d == "template_python_cmd":
+                    shutil.move(os.path.join(root, d), os.path.join(root, app_name_underscore))
         pyproject = os.path.join(tmpdir, "pyproject.toml")
         with open(pyproject, encoding="utf-8", mode="r") as pyproject_file:
-            pyproject_lines = pyproject_file.readlines()
+            pyproject_lines = pyproject_file.read().splitlines()
         for i, line in enumerate(pyproject_lines):
             if line.startswith("name ="):
                 pyproject_lines[i] = f'name = "{app_name}"'
@@ -52,12 +76,30 @@ def do_create_python_app(
                 pyproject_lines[i] = f'version = "{version}"'
             if line.startswith("authors ="):
                 pyproject_lines[i] = f'authors = ["{app_author}"]'
+            if command_name is None:
+                if line.startswith("[project.scripts]"):
+                    pyproject_lines[i] = ""
+                if line.startswith("test_cmd ="):
+                    pyproject_lines[i] = ""
+            else:
+                if line.startswith("test_cmd ="):
+                    pyproject_lines[i] = f'{command_name} = "{app_name_underscore}.cli:main"'
+        pyproject_lines = remove_double_blank_lines(pyproject_lines)
         with open(pyproject, encoding="utf-8", mode="w") as pyproject_file:
-            pyproject_file.writelines(pyproject_lines)
+            pyproject_file.write("\n".join(pyproject_lines))
+        test_cli = os.path.join(tmpdir, "tests", "test_cli.py")
+        with open(test_cli, encoding="utf-8", mode="r") as test_file:
+            test_lines = test_file.read().splitlines()
+        for i, line in enumerate(test_lines):
+            if line.startswith('COMMAND = "test_cmd"'):
+                new_line = f'COMMAND = "{command_name}"'
+                test_lines[i] = new_line
+        with open(test_cli, encoding="utf-8", mode="w") as test_file:
+            test_file.write("\n".join(test_lines))
         # change the url in setup.py
         setup = os.path.join(tmpdir, "setup.py")
         with open(setup, encoding="utf-8", mode="r") as setup_file:
-            setup_lines = setup_file.readlines()
+            setup_lines = setup_file.read().splitlines()
         for i, line in enumerate(setup_lines):
             if line.startswith("URL ="):
                 setup_lines[i] = f'URL = "{github_url}"'
@@ -65,15 +107,20 @@ def do_create_python_app(
             if line.startswith("maintainer="):
                 setup_lines[i] = f'maintainer="{app_author}"'
         with open(setup, encoding="utf-8", mode="w") as setup_file:
-            setup_file.writelines(setup_lines)
-        # remove the .git directory
-        rmtree(os.path.join(tmpdir, ".git"))
+            setup_file.write("\n".join(setup_lines))
+        files = os.listdir(tmpdir)
+        files = [os.path.join(tmpdir, f) for f in files if f != ".git"]
+        for f in files:
+            if os.path.isdir(f):
+                shutil.copytree(f, os.path.join(cwd, os.path.basename(f)))
+            else:
+                shutil.copy(f, cwd)
 
 
 def create_python_app() -> None:
     """Create a Python application."""
     # check if git exists
-    if not which("git"):
+    if not shutil.which("git"):
         raise RuntimeError("Git is not installed.")
     app_name = input("Python app name: ")
     check_name(app_name)
@@ -84,15 +131,17 @@ def create_python_app() -> None:
     if not version:
         version = "1.0.0"
     check_semantic_version(version)
-    add_command = input("Add a command? [y/N]: ")
-    add_command = add_command.lower() == "y" or add_command == ""
+    add_command = input("Add a command? [y/N]: ").lower() == "y"
+    command_name = None
+    if add_command:
+        command_name = input("Command name: ")
+        check_name(command_name)
     do_create_python_app(
-        app_name=app_name,
         app_description=app_description,
         app_author=app_author,
         version=version,
         github_url=github_url,
-        add_commmand=add_command,
+        command_name=command_name,
     )
 
 
---
+++ tests/test_createapp.py
@@ -0,0 +1,45 @@
+"""
+Unit test file.
+"""
+import os
+import unittest
+import shutil
+
+from createpythonapp.createapp import do_create_python_app
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+class CreateAppTester(unittest.TestCase):
+    """Main tester class."""
+
+    def test_imports(self) -> None:
+        """Test command line interface (CLI)."""
+
+        outdir = os.path.normpath(os.path.join(HERE, "..", ".MyAppTest"))
+        if os.path.exists(outdir):
+            shutil.rmtree(outdir)
+        do_create_python_app(
+            app_description="MyAppTest description",
+            app_author="Firstname Lastname",
+            version="1.2.3",
+            github_url="https://github.com/author/my-app",
+            command_name="mytestcommand",
+            cwd=outdir,
+        )
+        self.assertTrue(os.path.exists(outdir))
+        self.assertTrue(os.path.exists(os.path.join(outdir, "pyproject.toml")))
+        self.assertTrue(os.path.exists(os.path.join(outdir, "setup.py")))
+        self.assertTrue(os.path.exists(os.path.join(outdir, "src", "my_app")))
+        self.assertTrue(os.path.exists(os.path.join(outdir, "src", "my_app", "cli.py")))
+        self.assertTrue(os.path.exists(os.path.join(outdir, "src", "my_app", "__init__.py")))
+        self.assertTrue(os.path.exists(os.path.join(outdir, "tests")))
+        self.assertTrue(os.path.exists(os.path.join(outdir, "tests", "test_cli.py")))
+        # tox
+        self.assertTrue(os.path.exists(os.path.join(outdir, "tox.ini")))
+        rtn = os.system("tox")
+        self.assertEqual(rtn, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
